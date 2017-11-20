b5bd6d509b1af7c06c8de9d9da774d5ce0ab4575
Jade Allen Cook <jcook@deseretnews.com>
2017-11-20 16:46:19 -0700

    ⚗️ Added base functionality

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,56 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-	}
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+			// cache current page info
+			const link = window.location.href;
+			let title = '';
+			// get page header text if available
+			if (document.querySelector('h1')) title = document.querySelector('h1').innerText;
+			// gmail module
+			if (window.location.host === 'mail.google.com') {
+				// main mail window
+				if (window.location.search.length === 0) {
+					// add dump btn to gmail
+					dumpBtnHTML = '<div class="T-I J-J5-Ji T-I-KE L3" style="margin-top: 5px;" id="dump-btn">News List</div>';
+					document.querySelector('.z0').innerHTML += dumpBtnHTML;
+					// dump btn event listener
+					document.getElementById('dump-btn').addEventListener('click', () => {
+						// format date
+						const currentDate = new Date();
+						const month = currentDate.getMonth() + 1,
+							day = currentDate.getDate(),
+							year = currentDate.getFullYear();
+						// create subject
+						const subject = 'The Morning Links (' + month + '/' + day + '/' + year + ')';
+						window.location = 'https://mail.google.com/mail/u/0/?view=cm&su=' + subject;
+					});
+				} else { // dumping email
+					const insertBody = setInterval(() => {
+						// wait for email container to show
+						if (document.querySelector('.editable')) {
+							clearInterval(insertBody);
+							// insert email body
+							let output = '';
+							chrome.storage.sync.get('articles', (data) => {
+								for (let article of data.articles) {
+									output += article.title + '\n';
+									output += article.link + '\n\n';
+								}
+								document.querySelector('.editable').innerText = output;
+							});
+						}
+					}, 100);
+				}
+			} else {
+				// cache current page to use via page_action
+				chrome.storage.sync.set({
+					'current': {
+						'link': link,
+						'title': title
+					}
+				}, () => {});
+			}
+		}
 	}, 10);
 });
---
+++ src/page_action/app.js
@@ -0,0 +1,58 @@
+// cache extension elements
+const titleInputElem = document.getElementById('title'),
+  linkInputElem = document.getElementById('link'),
+  addBtn = document.getElementById('add'),
+  clearBtn = document.getElementById('clear'),
+  dumpBtn = document.getElementById('dump'),
+  articleType = document.getElementById('type'),
+  articleCountElem = document.getElementById('articles');
+
+// build vars
+let articles = [];
+chrome.storage.sync.get('articles', (data) => {
+  if (!data.articles) articles = [];
+  else articles = data.articles;
+  setArticleNum();
+});
+
+// set article text 
+function setArticleNum() {
+  if (articles.length === 1) articleCountElem.innerText = articles.length + ' Article';
+  else articleCountElem.innerText = articles.length + ' Articles';
+}
+
+// current page info
+chrome.storage.sync.get('current', (data) => {
+  const current = data.current;
+  titleInputElem.value = current.title;
+  linkInputElem.value = current.link;
+});
+
+// add button
+addBtn.addEventListener('click', () => {
+  articles.push({
+    title: titleInputElem.value,
+    link: linkInputElem.value,
+    type: articleType.value
+  })
+  chrome.storage.sync.set({
+    'articles': articles
+  }, () => {
+    setArticleNum();
+  });
+});
+
+// clear btn
+clearBtn.addEventListener('click', () => {
+  chrome.storage.sync.set({
+    'articles': []
+  }, () => {
+    articles = [];
+    setArticleNum();
+  });
+});
+
+// dev options
+chrome.storage.onChanged.addListener((data) => {
+  console.log(data);
+});
