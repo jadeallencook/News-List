17f2f35d3e4871a074490db3c112a93332945bc3
Jade Allen Cook <jcook@deseretnews.com>
2017-11-21 12:45:53 -0700

    📫 Final email html templating for output

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -31,13 +31,51 @@ chrome.extension.sendMessage({}, function (response) {
 						if (document.querySelector('.editable')) {
 							clearInterval(insertBody);
 							// insert email body
-							let output = '';
+							let output = {},
+							types = [];
 							chrome.storage.sync.get('articles', (data) => {
+								// get content for each section
 								for (let article of data.articles) {
-									output += article.title + '\n';
-									output += article.link + '\n\n';
+									// if output does not have type
+									if (!output[article.type]) {
+										// add to output/types
+										output[article.type] = '';
+										types.push(article.type);
+									}
+									// generate html for output
+									output[article.type] += article.title + '<br>';
+									output[article.type] += '<a href="' + article.link + '" target="_blank">' + article.link + '</a><br><br>';
 								}
-								document.querySelector('.editable').innerText = output;
+								// create space below signature
+								document.querySelector('.editable').innerHTML += '<br><br>';
+								// add section title/icon
+								for (let type of types) {
+									if (type === 'general') {
+										document.querySelector('.editable').innerHTML += '<font color="#ff0000" size="4"><b>GENERAL NEWS&nbsp;<img goomoji="1f4f0" data-goomoji="1f4f0" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4f0" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'politics') {
+										document.querySelector('.editable').innerHTML += '<font color="#666666" size="4"><b>POLITICS&nbsp;<img goomoji="fe4e6" data-goomoji="fe4e6" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/fe4e6" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'industry') {
+										document.querySelector('.editable').innerHTML += '<font color="#660000" size="4"><b>IN THE INDUSTRY&nbsp;<img goomoji="1f5de" data-goomoji="1f5de" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f5de" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'beehive') {
+										document.querySelector('.editable').innerHTML += '<font color="#3d85c6" size="4"><b>BEEHIVE STATE&nbsp;<img goomoji="1f41d" data-goomoji="1f41d" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f41d" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'deep') {
+										document.querySelector('.editable').innerHTML += '<font color="#38761d" size="4"><b>DEEP DIVES&nbsp;<img goomoji="1f4dd" data-goomoji="1f4dd" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4dd" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'family') {
+										document.querySelector('.editable').innerHTML += '<font color="#9900ff" size="4"><b>FAMILY AND HEALTH&nbsp;<img goomoji="1f34e" data-goomoji="1f34e" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f34e" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'faith') {
+										document.querySelector('.editable').innerHTML += '<font color="#073763" size="4"><b>FAITH&nbsp;<img goomoji="1f64f" data-goomoji="1f64f" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f64f" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'pop') {
+										document.querySelector('.editable').innerHTML += '<font color="#e06666" size="4"><b><POP CULTURE&nbsp;<img goomoji="1f4fa" data-goomoji="1f4fa" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4fa" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'money') {
+										document.querySelector('.editable').innerHTML += '<font color="#274e13" size="4"><b>MONEY&nbsp;<img goomoji="1f4b0" data-goomoji="1f4b0" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4b0" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'sports') {
+										document.querySelector('.editable').innerHTML += '<font color="#ff9900" size="4"><b>SPORTS&nbsp;<img goomoji="26bd" data-goomoji="26bd" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/26bd" class="CToWUd"></b></font><br><br>';
+									} else if (type === 'tech') {
+										document.querySelector('.editable').innerHTML += '<font color="#000000" size="4"><b>TECH&nbsp;<img goomoji="1f4f1" data-goomoji="1f4f1" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4f1" class="CToWUd"></b></font><br><br>';
+									}
+									// add section content
+									document.querySelector('.editable').innerHTML += output[type];
+								}
 							});
 						}
 					}, 100);
@@ -49,7 +87,7 @@ chrome.extension.sendMessage({}, function (response) {
 						'link': link,
 						'title': title
 					}
-				}, () => {});
+				});
 			}
 		}
 	}, 10);
