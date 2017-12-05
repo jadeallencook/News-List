fa42f2e75ec7df80926217d8f7ac376cc84812f6
Jade Allen Cook <jcook@deseretnews.com>
2017-12-05 11:16:21 -0700

    🔗 Mail no longer includes undefined types

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -32,42 +32,45 @@ chrome.extension.sendMessage({}, function (response) {
 							clearInterval(insertBody);
 							// insert email body
 							let output = {},
-							types = [];
+								types = [];
 							chrome.storage.local.get('articles', (data) => {
 								// get content for each section
 								for (let article of data.articles) {
 									// generate html for output
+									if (!output[article.type]) output[article.type] = '';
 									output[article.type] += article.title + '<br>';
 									output[article.type] += '<a href="' + article.link + '" target="_blank">' + article.link + '</a><br><br>';
 								}
 								// add section title/icon
 								let emailHTML = '';
 								for (let type of ['general', 'politics', 'industry', 'beehive', 'deep', 'family', 'faith', 'pop', 'sports', 'money', 'tech']) {
-									if (type === 'general') {
-										emailHTML += '<font color="#ff0000" size="4"><b>GENERAL NEWS&nbsp;<img goomoji="1f4f0" data-goomoji="1f4f0" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4f0" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'politics') {
-										emailHTML += '<font color="#666666" size="4"><b>POLITICS&nbsp;<img goomoji="fe4e6" data-goomoji="fe4e6" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/fe4e6" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'industry') {
-										emailHTML += '<font color="#660000" size="4"><b>IN THE INDUSTRY&nbsp;<img goomoji="1f5de" data-goomoji="1f5de" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f5de" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'beehive') {
-										emailHTML += '<font color="#3d85c6" size="4"><b>BEEHIVE STATE&nbsp;<img goomoji="1f41d" data-goomoji="1f41d" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f41d" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'deep') {
-										emailHTML += '<font color="#38761d" size="4"><b>DEEP DIVES&nbsp;<img goomoji="1f4dd" data-goomoji="1f4dd" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4dd" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'family') {
-										emailHTML += '<font color="#9900ff" size="4"><b>FAMILY AND HEALTH&nbsp;<img goomoji="1f34e" data-goomoji="1f34e" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f34e" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'faith') {
-										emailHTML += '<font color="#073763" size="4"><b>FAITH&nbsp;<img goomoji="1f64f" data-goomoji="1f64f" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f64f" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'pop') {
-										emailHTML += '<font color="#e06666" size="4"><b>POP CULTURE&nbsp;<img goomoji="1f4fa" data-goomoji="1f4fa" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4fa" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'money') {
-										emailHTML += '<font color="#274e13" size="4"><b>MONEY&nbsp;<img goomoji="1f4b0" data-goomoji="1f4b0" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4b0" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'sports') {
-										emailHTML += '<font color="#ff9900" size="4"><b>SPORTS&nbsp;<img goomoji="26bd" data-goomoji="26bd" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/26bd" class="CToWUd"></b></font><br><br>';
-									} else if (type === 'tech') {
-										emailHTML += '<font color="#000000" size="4"><b>TECH&nbsp;<img goomoji="1f4f1" data-goomoji="1f4f1" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4f1" class="CToWUd"></b></font><br><br>';
+									if (output[type]) {
+										if (type === 'general') {
+											emailHTML += '<font color="#ff0000" size="4"><b>GENERAL NEWS&nbsp;<img goomoji="1f4f0" data-goomoji="1f4f0" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4f0" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'politics') {
+											emailHTML += '<font color="#666666" size="4"><b>POLITICS&nbsp;<img goomoji="fe4e6" data-goomoji="fe4e6" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/fe4e6" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'industry') {
+											emailHTML += '<font color="#660000" size="4"><b>IN THE INDUSTRY&nbsp;<img goomoji="1f5de" data-goomoji="1f5de" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f5de" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'beehive') {
+											emailHTML += '<font color="#3d85c6" size="4"><b>BEEHIVE STATE&nbsp;<img goomoji="1f41d" data-goomoji="1f41d" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f41d" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'deep') {
+											emailHTML += '<font color="#38761d" size="4"><b>DEEP DIVES&nbsp;<img goomoji="1f4dd" data-goomoji="1f4dd" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4dd" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'family') {
+											emailHTML += '<font color="#9900ff" size="4"><b>FAMILY AND HEALTH&nbsp;<img goomoji="1f34e" data-goomoji="1f34e" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f34e" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'faith') {
+											emailHTML += '<font color="#073763" size="4"><b>FAITH&nbsp;<img goomoji="1f64f" data-goomoji="1f64f" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f64f" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'pop') {
+											emailHTML += '<font color="#e06666" size="4"><b>POP CULTURE&nbsp;<img goomoji="1f4fa" data-goomoji="1f4fa" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4fa" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'money') {
+											emailHTML += '<font color="#274e13" size="4"><b>MONEY&nbsp;<img goomoji="1f4b0" data-goomoji="1f4b0" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4b0" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'sports') {
+											emailHTML += '<font color="#ff9900" size="4"><b>SPORTS&nbsp;<img goomoji="26bd" data-goomoji="26bd" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/26bd" class="CToWUd"></b></font><br><br>';
+										} else if (type === 'tech') {
+											emailHTML += '<font color="#000000" size="4"><b>TECH&nbsp;<img goomoji="1f4f1" data-goomoji="1f4f1" style="margin:0 0.2ex;vertical-align:middle;max-height:24px" src="https://mail.google.com/mail/e/1f4f1" class="CToWUd"></b></font><br><br>';
+										}
+										// add section content
+										emailHTML += output[type];
 									}
-									// add section content
-									emailHTML += output[type];
 								}
 								let emailSignatureHTML = document.querySelector('.editable').innerHTML;
 								document.querySelector('.editable').innerHTML = emailHTML + emailSignatureHTML;
